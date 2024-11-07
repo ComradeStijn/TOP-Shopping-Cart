68cc0f8e582cd1c8d5f52affbfb3a6bdedb0eacd
Stijn Servaes <142609421+ComradeStijn@users.noreply.github.com>
2024-11-07 18:49:00 +0100

    feat: Product cards on Home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,35 +1,45 @@
 import styled from "styled-components";
 import Hero from "./Hero";
+import Card from "./Card";
 import { Fade } from "react-awesome-reveal";
+import { useLoaderData } from "react-router-dom";
 
 export default function Home() {
+  const data = useLoaderData();
+
   return (
     <main>
-      <Fade delay={500}  direction="left" triggerOnce={true}><Hero /></Fade>
-      <Container>
-        <Bestsellers>Our Bestsellers</Bestsellers>
-
-        <Grid>
-          <Testbox /><Testbox /><Testbox /><Testbox /><Testbox /><Testbox /><Testbox /><Testbox />
-        </Grid>
-      </Container>
+      <Fade delay={500} direction="left" triggerOnce={true}>
+        <Hero />
+      </Fade>
+      <Fade delay={1000} triggerOnce={true} direction="up">
+        <Container>
+          <Bestsellers>Our Bestsellers</Bestsellers>
+          <Grid>
+            {data.map((product) => (
+              <Fade key={product.id} triggerOnce={true} cascade={true} damping={0.5} direction="up">
+                <Card product={product} />
+              </Fade>
+            ))}
+          </Grid>
+        </Container>
+      </Fade>
     </main>
   );
 }
 
 const Bestsellers = styled.h1`
   text-align: center;
-  font-family: 'Anton SC', sans-serif;
+  font-family: "Anton SC", sans-serif;
   font-weight: 300;
   font-size: 3rem;
   margin: 1rem auto;
-`
+`;
 
 const Container = styled.div`
-  box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
   margin: 2rem auto;
   padding: 2rem;
-
 
   @media (min-width: ${(props) => props.theme.break.tablet}) {
     width: 760px;
@@ -50,10 +60,4 @@ const Grid = styled.section`
   @media (min-width: ${(props) => props.theme.break.desktop}) {
     grid-template-columns: repeat(4, 1fr);
   }
-`
-
-const Testbox = styled.div`
-  height: 200px;
-  background-color: red;
-  outline: 1px solid blue;
-`
+`;
